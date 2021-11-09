d452d3de6cd1f4c118337852b141bf743b0cd754
lucky-jack <luckychipdev@protonmail.com>
2021-11-09 09:54:26 +0800

    test LCToken

--- test/LCToken.test.js
+++ test/LCToken.test.js
@@ -2,14 +2,15 @@ const { assert } = require("chai");
 
 const LCToken = artifacts.require('LCToken');
 
-contract('LCToken', ([alice, bob, carol, dev, minter]) => {
+contract('LCToken', ([alice, bob, carol, dev, creater]) => {
     beforeEach(async () => {
-        this.lc = await LCToken.new({ from: minter });
+        this.lc = await LCToken.new({ from: creater });
+        await this.lc.addMinter(creater, {from: creater});
     });
 
 
     it('mint', async () => {
-        await this.lc.mint(alice, 1000, { from: minter });
+        await this.lc.mint(alice, 1000, { from: creater });
         assert.equal((await this.lc.balanceOf(alice)).toString(), '1000');
     })
 });
