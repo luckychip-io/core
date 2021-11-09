188675bcad4cc7f317ec520385893d305c65d279
lucky-jack <luckychipdev@protonmail.com>
2021-11-09 17:32:04 +0800

    test MasterChef done

--- test/MasterChef.test.js
+++ test/MasterChef.test.js
@@ -1,40 +1,43 @@
 const { expectRevert, time } = require('@openzeppelin/test-helpers');
-const { AddressZero } = require("@ethersproject/constants")
 const { assert } = require('chai');
+const { AddressZero } = require("@ethersproject/constants")
+
 const LCToken = artifacts.require('LCToken');
 const MasterChef = artifacts.require('MasterChef');
 const MockBEP20 = artifacts.require('libs/MockBEP20');
-//let perBlock = '100000000000000000000';
+const Referral = artifacts.require('Referral');
+
 let perBlock = '1000';
 
-contract('MasterChef', ([alice, bob, carol, dev0, dev1, dev2, safuAddr, treasuryAddr, minter]) => {
+contract('MasterChef', ([alice, bob, carol, dev0, dev1, dev2, ecoAddr, treasuryAddr, creater]) => {
     beforeEach(async () => {
-        this.lc = await LCToken.new({ from: minter });
+        this.lc = await LCToken.new({ from: creater });
     
-        this.lp1 = await MockBEP20.new('LPToken', 'LP1', '1000000', { from: minter });
-        this.lp2 = await MockBEP20.new('LPToken', 'LP2', '1000000', { from: minter });
-        this.lp3 = await MockBEP20.new('LPToken', 'LP3', '1000000', { from: minter });
+        this.lp1 = await MockBEP20.new('LPToken', 'LP1', '1000000', { from: creater });
+        this.lp2 = await MockBEP20.new('LPToken', 'LP2', '1000000', { from: creater });
+        this.lp3 = await MockBEP20.new('LPToken', 'LP3', '1000000', { from: creater });
         
-        this.chef = await MasterChef.new(this.lc.address, dev0, dev1, dev2, safuAddr, treasuryAddr, perBlock, '100', '900000','32400', '24300', '24300', '10000', { from: minter });
-        await this.lc.transferOwnership(this.chef.address, { from: minter });
+        this.chef = await MasterChef.new(this.lc.address, dev0, dev1, dev2, ecoAddr, treasuryAddr, perBlock, '100', '7000', '96', '72', '72', '60', '25', { from: creater });
+        await this.lc.addMinter(this.chef.address, { from: creater });
     
-        await this.lp1.transfer(alice, '2000', { from: minter });
-        await this.lp2.transfer(alice, '2000', { from: minter });
-        await this.lp3.transfer(alice, '2000', { from: minter });
+        this.referral = await Referral.new(this.lc.address, {from: creater});
+        await this.referral.addOperator(this.chef.address, {from: creater});
+        await this.chef.setReferral(this.referral.address, {from: creater});
 
-        await this.lp1.transfer(bob, '2000', { from: minter });
-        await this.lp1.transfer(carol, '2000', { from: minter });
-        await this.lp2.transfer(bob, '2000', { from: minter });
-        await this.lp2.transfer(carol, '2000', { from: minter });
-       // await this.lp3.transfer(bob, '2000', { from: minter });
+        await this.lp1.transfer(alice, '2000', { from: creater });
+        await this.lp2.transfer(alice, '2000', { from: creater });
+        await this.lp3.transfer(alice, '2000', { from: creater });
+
+        await this.lp1.transfer(bob, '2000', { from: creater });
+        await this.lp1.transfer(carol, '2000', { from: creater });
+        await this.lp2.transfer(bob, '2000', { from: creater });
+        await this.lp2.transfer(carol, '2000', { from: creater });
+       // await this.lp3.transfer(bob, '2000', { from: creater });
     });
     it('real case', async () => {
-        await this.chef.add('1000', '0', this.lp1.address, { from: minter });
+        await this.chef.add('1000', this.lp1.address, { from: creater });
 		assert.equal((await this.chef.poolLength()).toString(), "1");
 		assert.equal((await this.chef.totalAllocPoint()).toString(), "1000");
-		assert.equal((await this.chef.totalBonusPoint()).toString(), "0");
-        await this.chef.addBonus(this.lp3.address, { from: minter });
-		assert.equal((await this.chef.bonusLength()).toString(), "1");
 		console.log(`startBlock: ${(await this.chef.startBlock())}`);
 
         //1 - lp
@@ -44,7 +47,7 @@ contract('MasterChef', ([alice, bob, carol, dev0, dev1, dev2, safuAddr, treasury
 		assert.equal((await this.lc.balanceOf(alice)).toString(), '0');
         await time.advanceBlockTo('150');
 		console.log(`Current block: ${(await time.latestBlock())}`);
-        await this.chef.deposit(0, '20', AddressZero, { from: alice });
+        await this.chef.deposit(0, '20', bob, { from: alice });
 		let user = await this.chef.userInfo(0, alice, {from: alice});
 		console.log(`${user[0]},${user[1]}`);
 		console.log((await this.chef.pendingLC(0, alice)).toString());
@@ -57,6 +60,7 @@ contract('MasterChef', ([alice, bob, carol, dev0, dev1, dev2, safuAddr, treasury
         await this.chef.withdraw(0, '20', { from: alice });
 		user = await this.chef.userInfo(0, alice, {from: alice});
 		console.log(`${user[0]},${user[1]}`);
+        await this.chef.claimLC(0, {from: alice});
         let aliceBalance = await this.lc.balanceOf(alice);
         console.log('alice balance: ', aliceBalance.toString());
 		console.log(`Current block: ${(await time.latestBlock())}`);
@@ -64,62 +68,22 @@ contract('MasterChef', ([alice, bob, carol, dev0, dev1, dev2, safuAddr, treasury
         console.log('---------------');
 
         await time.advanceBlockTo('210'); 
-        await this.chef.withdrawDevFee({ from: minter });
+        await this.chef.withdrawDevFee({ from: creater });
         let balanceDev0 = await this.lc.balanceOf(dev0);
         console.log('dev0 address balance: ', balanceDev0.toString());
         let balanceDev1 = await this.lc.balanceOf(dev1);
         console.log('dev1 address balance: ', balanceDev1.toString());
         let balanceDev2 = await this.lc.balanceOf(dev2);
         console.log('dev2 address balance: ', balanceDev2.toString());
-        let balanceSafu = await this.lc.balanceOf(safuAddr);
-        console.log('safu address balance: ', balanceSafu.toString());
+        let balanceEco = await this.lc.balanceOf(ecoAddr);
+        console.log('eco address balance: ', balanceEco.toString());
         let balanceTreasury = await this.lc.balanceOf(treasuryAddr);
         console.log('treasury address balance: ', balanceTreasury.toString());
+
+        result = await this.referral.getReferralCommission.call(bob);
+        console.log(`commission,${result[0]},${result[1]},${result[2]},${result[3]},${result[4]},${result[5]}`);
+        assert.equal((await this.lc.balanceOf(bob)).toString(), '0');
+        await this.referral.claimLpCommission({from: bob});
+        assert.equal((await this.lc.balanceOf(bob)).toString(), '1750');
     })
-    it('bonus', async () => {
-        await this.chef.add('1000', '90', this.lp1.address, { from: minter });
-		assert.equal((await this.chef.poolLength()).toString(), "1");
-		pool = await this.chef.poolInfo(0, {from:minter});
-		console.log(`${pool[0]},${pool[1]},${pool[2]},${pool[3]},${pool[4]}`);
-        await this.chef.addBonus(this.lp3.address, { from: minter });
-		assert.equal((await this.chef.bonusLength()).toString(), "1");
-		bonusPool = await this.chef.bonusInfo(0, {from:minter});
-		console.log(`${bonusPool[0]},${bonusPool[1]}`);
-		console.log((await this.chef.totalBonusPoint()).toString());
-
-        await this.lp1.approve(this.chef.address, '1000000', { from: alice });
-        await this.lp3.approve(this.chef.address, '1000000', { from: minter });
-        await this.lc.approve(this.chef.address, '1000000', { from: alice });
-        await this.lp3.approve(this.chef.address, '1000000', { from: alice });
-		assert.equal((await this.lp3.balanceOf(alice)).toString(), '2000');
-		bonusPerShare = await this.chef.poolBonusPerShare(0, 0);
-		console.log(`${bonusPerShare}`);
-        await time.advanceBlockTo('300');
-		console.log(`Current block: ${(await time.latestBlock())}`);
-        await this.chef.deposit(0, '20', AddressZero, { from: alice });
-		await this.lp3.transfer(this.chef.address, '100', {from: minter});
-		await this.chef.updateBonus(0, { from: minter});
-		bonusPool = await this.chef.bonusInfo(0, {from:minter});
-		console.log(`${bonusPool[0]},${bonusPool[1]}`);
-		user = await this.chef.userInfo(0, alice, {from: alice});
-		console.log(`${user[0]},${user[1]}`);
-		userBonusDebt = await this.chef.userBonusDebt(0, alice, {from: alice});	
-		console.log(`${userBonusDebt}`);
-		bonusPerShare = await this.chef.poolBonusPerShare(0, 0);
-		console.log(`${bonusPerShare}`);
-		assert.equal((await this.lp3.balanceOf(this.chef.address)).toString(), '100');
-		pendingBonus = await this.chef.pendingBonus(0, alice);
-		console.log(`${pendingBonus[0]}`);
-		
-
-        await time.advanceBlockTo('350'); 
-        await this.chef.withdraw(0, '20', { from: alice });
-
-        let aliceBalance = await this.lc.balanceOf(alice);
-        console.log('alice balance: ', aliceBalance.toString());
-        aliceBalance = await this.lp3.balanceOf(alice);
-        console.log('alice lp3 balance: ', aliceBalance.toString());
-	
-	});
-
 });
