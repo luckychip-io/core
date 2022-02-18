350191c5a5092b6d5b622bad2847093b6f3aec0f
lucky-jack <luckychipdev@protonmail.com>
2022-02-18 11:47:08 +0800

    update MockBEP20.sol

--- contracts/token/MockBEP20.sol
+++ contracts/token/MockBEP20.sol
@@ -2,14 +2,14 @@
 
 pragma solidity 0.6.12;
 
-import "./LCBEP20.sol";
+import "./BEP20.sol";
 
-contract MockBEP20 is LCBEP20 {
+contract MockBEP20 is BEP20 {
     constructor(
         string memory name,
         string memory symbol,
         uint256 supply
-    ) public LCBEP20(name, symbol) {
+    ) public BEP20(name, symbol) {
         _mint(msg.sender, supply);
     }
 }
